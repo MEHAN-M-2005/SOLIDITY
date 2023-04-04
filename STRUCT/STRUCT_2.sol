c0b76b4f42ad1d9515c226a3bd93b6325dbf74e5
MEHAN.M <115608700+MEHAN-M-2005@users.noreply.github.com>
2023-04-04 15:44:53 +0530

    Add files via upload

---
+++ STRUCT/STRUCT_2.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.7;
+contract NestedMapping {
+    //USING STRUCT STORING MULTIPLE VALUES IN DIFFERENT DATA TYPES IN AN SINGLE INDEX OF AN ARRAY
+    struct bio_data{
+        string name;
+        uint age;
+    }
+    bio_data[] public bio;
+    bio_data[] public bio1;
+    function me(string memory _name, uint _age) public{
+        bio.push(bio_data(_name,_age));
+    } 
+    function mee(string memory _namee,uint _agee) public 
+    {
+        bio.push(bio_data(_namee,_agee));
+    }
+}
