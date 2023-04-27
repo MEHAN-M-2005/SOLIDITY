34ad7ccdf559059cb0b521046ab5c5b7a0ce463d
MEHAN.M <115608700+MEHAN-M-2005@users.noreply.github.com>
2023-04-27 20:23:15 +0530

    Update mapping.sol

--- mapping.sol
+++ mapping.sol
@@ -1,11 +1,25 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.0;
-contract mehan{
-    //MAPPING 
-    mapping(uint => string) public bio_data;
-    function mee(uint no, string memory name) public{
-        bio_data[no]=name;
-    } 
+
+contract Mapping {
+    // Mapping from address to uint
+    mapping(address => uint) public myMap;
+
+    function get(address _addr) public view returns (uint) {
+        // Mapping always returns a value.
+        // If the value was never set, it will return the default value.
+        return myMap[_addr];
+    }
+
+    function set(address _addr, uint _i) public {
+        // Update the value at this address
+        myMap[_addr] = _i;
+    }
+
+    function remove(address _addr) public {
+        // Reset the value to the default value.
+        delete myMap[_addr];
+    }
 }
 contract NestedMapping {
     // Nested mapping (mapping from address to another mapping)
